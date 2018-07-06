1f77382893df7034d33e0fb67be80523927f7021
Zachary Blank <zacharyblank@zacharys-imac.lan>
2018-07-06 13:51:05 -0700

    improved TSP

--- app.js
+++ app.js
@@ -33,48 +33,72 @@ app.get('/route/:coordinates', function(req, res) {
 		return res.json({"error": err});
 	}
 
+	console.log("Input: ", coordinates.length)
+
 	if (req.query.optimize === 'true') {
 		osrm.table({
 			coordinates: coordinates,
 		}, function(err, result) {
 			if (err) return res.json({"error":err.message});
 
-			var end = Array()
-			result.durations.push(end)
 
+			var dummyNodeEdges = []
 			for (var i = 0; i < result.durations.length; i++) {
-				if (i == 0 || i == result.durations.length - 1) {
-					end.push(0)
-					result.durations[i].push(0)
+				if (i == result.durations.length - 1) {
+					dummyNodeEdges.push(0)
 				} else {
-					end.push(9999)
-					result.durations[i].push(999999999)
+					// dummyNodeEdges.push(Number.MAX_VALUE)
+					dummyNodeEdges.push(999999999)
 				}
+
+				result.durations[i].push(dummyNodeEdges[dummyNodeEdges.length-1])
 			}
 
-			result.durations[result.durations.length-1] = end
+			dummyNodeEdges.push(0)
+			dummyNodeEdges[0] = 0
+			result.durations.push(dummyNodeEdges)
+
+			console.log("Durration Length: ", result.durations.length)
+			console.log("DUMMY: ", dummyNodeEdges)
+
+			// console.log("RESULTS: ", result.durations)
 
 			var TSP = new ortools.TSP({
-				numNodes: result.durations[0].length,
+				numNodes: result.durations.length,
 				costs: result.durations
 			});
 
 			TSP.Solve({
 				computeTimeLimit: 10000,
-				depotNode: 0
+				depotNode: 0,
 			}, function(err, solution) {
+				var ordered = [coordinates[0]]
 
-				var ordered = Array()
-				for (var i = 1; i < solution.length; i++) {
-					ordered.push(coordinates[solution[i]])
+				console.log("Solution: ", solution.length, solution)
+
+				for (var i = 0; i < solution.length; i++) {
+					// skip the last dummy node!
+					if (solution[i] < coordinates.length-1) {
+						ordered.push(coordinates[solution[i]])
+					}
 				}
 
 				ordered.push(coordinates[coordinates.length-1])
-				ordered.unshift(coordinates[0])
-				// ordered.push(coordinates[coordinates.length-1])
+
+				// ordered.splice(-1,1)
+				// solution.splice(-1,1)
+
+				// console.log("Solution: ", solution.length)
+				// console.log("Output: ", ordered.length)
+
 				// console.log(ordered)
 
-				// TODO: add the first and last
+				// solution = solution.splice(1, solution.length-1).reverse()
+				solution.unshift(0)
+				solution.pop()
+				// solution = solution.map(function(i) {
+				// 	return i-1
+				// })
 
 				osrm.route({
 					coordinates: ordered,
